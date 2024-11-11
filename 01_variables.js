9778b2a72547c65d8923873fa0132627c13f4c93
chaitanya-173 <chaitanyachaudhary73@gmail.com>
2024-11-11 10:52:40 +0530

    commiting daily

---
+++ 01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553  //preferred
+let accountEmail = "chaitanyachaudhary73@gmail.com"  //preferred
+var accountPassword = "12345"
+accountCity = "Noida"  //shouldn't be used
+let accountState;
+
+accountEmail = "chaitanya23nov@gmail.com"
+accountPassword = "12121"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
