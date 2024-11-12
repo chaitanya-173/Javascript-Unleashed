d690c19e9d25640011e14ed930cf73ee80990a0c
chaitanya-173 <chaitanyachaudhary73@gmail.com>
2024-11-12 17:43:56 +0530

    commitment

---
+++ 07_numberAndMath.js
@@ -0,0 +1,41 @@
+// *********************** Numbers *************************
+
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);  // [Number: 100]
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));          // 100.00
+
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(4));  // 123.9 (round off value)
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString());         // 10,00,000
+console.log(hundreds.toLocaleString('en-IN'));  // 10,00,000
+
+
+// ********************** Maths *****************************
+
+console.log(Math);
+console.log(Math.abs(-4));      // 4 => absolute
+console.log(Math.round(4.6));   // 5 => round off
+console.log(Math.ceil(4.2));    // 5
+console.log(Math.floor(4.9));   // 4
+console.log(Math.sqrt(7.5));    // 2.7386127875258306
+console.log(Math.pow(5, 2));    // 25
+console.log(Math.min(4,3,6,8)); // 3
+console.log(Math.max(4,3,6,8)); // 8
+
+console.log(Math.random());     // always between 0 and 1
+console.log((Math.random()*10) + 1);  // between 1 to 10
+console.log(Math.floor(Math.random()*10) + 1);  // to get integer value
+
+const min = 1
+const max = 6
+
+// To generate random absolute numbers between minNum and maxNum
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
+
